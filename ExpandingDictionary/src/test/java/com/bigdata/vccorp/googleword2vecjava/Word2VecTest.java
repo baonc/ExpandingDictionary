2081c1b1f2a9cde34a5b94738310629c89c4d79d
BaoNC <baonc93@gmail.com>
2015-07-14 16:11:38 +0700

    test

--- ExpandingDictionary/src/test/java/com/bigdata/vccorp/googleword2vecjava/Word2VecTest.java
+++ ExpandingDictionary/src/test/java/com/bigdata/vccorp/googleword2vecjava/Word2VecTest.java
@@ -1,7 +1,5 @@
 package com.bigdata.vccorp.googleword2vecjava;
 
-import org.junit.rules.ExpectedException;
-
 /**
  * Class test Word2VecJava library
  * 
@@ -10,12 +8,5 @@ import org.junit.rules.ExpectedException;
  * @github https://github.com/baonc/ExpandingDictionary
  */
 public class Word2VecTest {
-	public ExpectedException expected = ExpectedException.none();
-	
-	// cleanup after test run
-	public void after() {
-		Thread.interrupted();
-	}
-	
-	
+		
 }
